6dedbe9cdc3b57897406eb5ece32a2d4295555b9
trishkare <emazasanura@gamil.com>
2018-08-10 11:17:20 +0300

    add if and alert for not a triangle

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,10 @@ var c = parseInt(prompt("enter length of side of c"))
 if(a==b && b==c)
 alert("Equilateral");
 
-else if((a==b && b!=c ) || (a!=b && c==a) || (c==b && c!=a) )alert("Isosceles");
+else if((a==b && b!=c ) || (a!=b && c==a) || (c==b && c!=a) )
+alert("Isosceles");
 
 else if(a!=b && b!=c && c!=a)alert("Scalene");
+
+else if(a >= (b+c) || c >= (b+a) || b >= (a+c) )
+alert("Not a triangle");
