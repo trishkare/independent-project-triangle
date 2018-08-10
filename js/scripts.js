59816f7c67e4f5aa4a16b2793b836171bd0fae0c
trishkare <emazasanura@gamil.com>
2018-08-10 12:29:27 +0300

    add functions to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,11 @@ var b= parseInt(prompt("enter length of side of b"))
 var a = parseInt(prompt("enter length of side of a"))
 var c = parseInt(prompt("enter length of side of c"))
 
-if(a==b && b==c)
+if(isNaN(a)===true ||isNaN(b)===true || isNaN(c)===true ){
+  alert("Ensure you enter a value");
+}
+
+else if(a==b && b==c)
 alert("Equilateral");
 
 else if((a==b && b!=c ) || (a!=b && c==a) || (c==b && c!=a) )
@@ -12,3 +16,8 @@ else if(a!=b && b!=c && c!=a)alert("Scalene");
 
 else if(a >= (b+c) || c >= (b+a) || b >= (a+c) )
 alert("Not a triangle");
+
+
+if(isNaN(a)===true ||isNaN(b)===true || isNaN(c)===true   ){
+  alert("Ensure you enter a value");
+}
